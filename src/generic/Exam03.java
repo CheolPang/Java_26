ab8c7f20bddac4af213b8160e0e1e1e921eda84c
CheolPang <cheolpang@proton.me>
2026-05-23 13:24:14 +0900

    Refactor: Add pair comparison utility method

--- src/generic/Exam03.java
+++ src/generic/Exam03.java
@@ -2,13 +2,18 @@ package generic;
 
 public class Exam03 {
 	public static void main(String[] args) {
-		Pair<Integer, String> p1 = new Pair<Integer, String>(1, "사과");
+		Pair<Integer, String> p1 = new Pair<>(1, "사과"); //<> 안 값 생략이 가능함. (사실 <>도 생략은 가능한데 보통 <>까지는 씀)
 		Pair<Integer, String> p2 = new Pair<Integer, String>(1, "사과");
+		Pair<Integer, String> p3 = new Pair<Integer, String>(1, "바나나");
 		
 		boolean result1 = p1 == p2;
 		System.out.println(result1); // 다른 주소값을 참조하기 때문에 false가 출력됨.
 		System.out.println(p1.equals(p2));
 		
-		
+		System.out.println("================");
+		boolean result2 = Util.compare(p1, p2);
+		System.out.println(result2);
+		boolean result3 = Util.compare(p1, p3);
+		System.out.println(result3);
 	}
 }
--- src/generic/Util.java
+++ src/generic/Util.java
@@ -7,4 +7,9 @@ public class Util {
 		present.setT(t);
 		return present;
 	}
+	public static <K,V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+		boolean keyCompare = p1.getKey().equals(p2.getKey());
+		boolean ValueCompare = p1.getValue().equals(p2.getValue());
+		return keyCompare && ValueCompare;
+	}
 }
